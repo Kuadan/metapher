3e0462abf70e6ce38fad599b718017e6b8c71763
Kuadan <tranphuongthao63@gmail.com>
2019-02-23 00:14:46 +0100

    test canvas

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -20,10 +20,20 @@ PennController(
         .settings.size(200,200) // Don't print it yet
         //.print()
     ,
-    newCanvas("tanks", 500, 200)
+    newImage("competitor2", "37_1_related_JPG.JPG")
+        .settings.size(200,200) // Don't print it yet
+        //.print()
+    ,
+    newImage("target2", "38_4_JPG.JPG")
+        .settings.size(200,200) // Don't print it yet
+        //.print()
+    ,
+    newCanvas("tanks", 400, 400)
         .settings.add(   0, 0, getImage("competitor") ) // 0 = left of canvas
-        .settings.add( 300, 0, getImage("target") )  // 300 = 100px to the right of the right edge of competitor
-        .print() // This prints the canvas, i.e. target and competitor side by side
+        .settings.add( 200, 0, getImage("target") )  // 300 = 100px to the right of the right edge of competitor
+        .settings.add(   0, 200, getImage("competitor2") ) // 0 = left of canvas
+        .settings.add( 200, 200, getImage("target2") )  // 300 = 100px to the right of the right edge of competitor
+    .print() // This prints the canvas, i.e. target and competitor side by side
     ,
     newSelector("tank")
         .settings.add( getImage("target") , getImage("competitor") )
