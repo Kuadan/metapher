fe2a86247624f6b96317c9edcc8c586d0ff7f559
Kuadan <tranphuongthao63@gmail.com>
2019-02-28 20:47:06 +0100

    test

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -11,34 +11,34 @@ PennController.Template( PennController.GetTable("testlist.csv") ,
         newText("test sentence", row.sentence)
             .print()
         ,
-        newImage("img1", row.picture1)
-            .settings.size(300,300)
-            .print()
-        ,
-        newImage("img2", row.picture2)
-            .settings.size(300,300)
-            .print()
-        ,
-        newImage("img3", row.picture3)
-            .settings.size(300,300)
-            .print()
-        ,
-        newImage("img4", row.picture4)
-            .settings.size(300,300)
-            .print()
-        ,
-        newSelector("tank")
-        .settings.add( getImage("img1"), getImage("img2"), getImage("img3"), getImage("img4") )
-        .wait()
-        // newCanvas("tanks", 600, 600)
-        //   .settings.add(   0, 0, getImage("picture1", row.picture1) ) // 0 = left of canvas
-        //   .settings.add( 300, 0, getImage("picture2", row.picture2) )  // 300 = 100px to the right of the right edge of competitor
-        //   .settings.add(   0, 300, getImage("picture3", row.picture3) ) // 0 = left of canvas
-        //   .settings.add( 300, 300, getImage("picture4", row.picture4) )  // 300 = 100px to the right of the right edge of competitor
-        //   .print()
+        // newImage("img1", row.picture1)
+        //     .settings.size(300,300)
+        //     .print()
+        // ,
+        // newImage("img2", row.picture2)
+        //     .settings.size(300,300)
+        //     .print()
+        // ,
+        // newImage("img3", row.picture3)
+        //     .settings.size(300,300)
+        //     .print()
+        // ,
+        // newImage("img4", row.picture4)
+        //     .settings.size(300,300)
+        //     .print()
         // ,
         // newSelector("tank")
-        //     .settings.add( getImage("picture1") , getImage("picture2"), getImage("picture3"), getImage("picture4") )
-        //     .wait()
+        // .settings.add( getImage("img1"), getImage("img2"), getImage("img3"), getImage("img4") )
+        // .wait()
+        newCanvas("tanks", 600, 600)
+          .settings.add(   0, 0, getImage("pic1", row.picture1) ) // 0 = left of canvas
+          .settings.add( 300, 0, getImage("pic2", row.picture2) )  // 300 = 100px to the right of the right edge of competitor
+          .settings.add(   0, 300, getImage("pic3", row.picture3) ) // 0 = left of canvas
+          .settings.add( 300, 300, getImage("pic4", row.picture4) )  // 300 = 100px to the right of the right edge of competitor
+          .print()
+        ,
+        newSelector("tank")
+            .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
+            .wait()
     )
 );
