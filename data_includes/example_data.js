49abad65fa50c19bb42b6b6652e81db30430981b
Kuadan <tranphuongthao63@gmail.com>
2019-03-04 14:22:34 +0100

    change canvas size

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -28,11 +28,11 @@ PennController.Template( PennController.GetTable("itemlist.csv") ,
         // newSelector("tank")
         // .settings.add( getImage("img1"), getImage("img2"), getImage("img3"), getImage("img4") )
         // .wait()
-        newCanvas("tanks", 600, 600)
+        newCanvas("tanks", 800, 800)
           .settings.add(   0, 0, newImage("pic1", row.picture1) ) // 0 = left of canvas
-          .settings.add( 300, 0, newImage("pic2", row.picture2) )  // 300 = 100px to the right of the right edge of competitor
-          .settings.add(   0, 300, newImage("pic3", row.picture3) ) // 0 = left of canvas
-          .settings.add( 300, 300, newImage("pic4", row.picture4) )  // 300 = 100px to the right of the right edge of competitor
+          .settings.add( 400, 0, newImage("pic2", row.picture2) )  // 300 = 100px to the right of the right edge of competitor
+          .settings.add(   0, 400, newImage("pic3", row.picture3) ) // 0 = left of canvas
+          .settings.add( 400, 400, newImage("pic4", row.picture4) )  // 300 = 100px to the right of the right edge of competitor
           .print()
         ,
         newSelector("tank")
