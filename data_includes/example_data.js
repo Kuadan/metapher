b1eadd7552d00ab3583929fe5750627611e7ec1a
Kuadan <tranphuongthao63@gmail.com>
2019-03-07 12:49:54 +0100

    timer

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -52,8 +52,11 @@ PennController.Template( PennController.GetTable("itemlist.csv") ,
         newSelector("tank")
           //.settings.log()
           .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
-          .wait()
-            getTimer("quick")
+        ,
+        getSeletor()
+        ,
+        getTimer("quick")
            .test.ended()
+           .
     )
 );
