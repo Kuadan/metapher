8313f22eaa17f9464be9a904ffa1060110cc638f
Kuadan <tranphuongthao63@gmail.com>
2019-03-07 12:36:05 +0100

    test timer

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -46,11 +46,15 @@ PennController.Template( PennController.GetTable("itemlist.csv") ,
           .settings.add( 700, 400, newImage("pic4", row.picture4) )  // 300 = 100px to the right of the right edge of competitor
           .print()
         ,
+        newTimer("quick", 4000)
+          .start()
+        ,
         newSelector("tank")
           //.settings.log()
           .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
           .wait()
-          .remove()
+            getTimer("quick")
+          
 
     )
 );
