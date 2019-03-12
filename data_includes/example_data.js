e6e8c958b16105bcd2446b0134656039b4325766
Kuadan <tranphuongthao63@gmail.com>
2019-03-12 12:46:31 +0100

    test

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -13,11 +13,11 @@ PennController("instructions" ,
     ,
     newText("intro4", "Press 1, 2, 3 or 4 to start.")
     ,
-    newKey("numeric", "1234ï¿½") // Handle exception: an all-digit string would be evaluated as a charCode
+    newButton("numeric", "weiter") // Handle exception: an all-digit string would be evaluated as a charCode
+        .print()
         .wait()                 // Start when 1, 2, 3 or 4 is pressed (weird character not on keyboard)
 )
-.setOption("hideProgressBar", true)
-);
+.setOption("hideProgressBar", true);
 
 PennController.Template( PennController.GetTable("itemlist.csv"),
     row => PennController( "picture" ,
