fb508732704a726146347173769d28be60112b0a
Kuadan <tranphuongthao63@gmail.com>
2019-03-12 11:15:51 +0100

    test example

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,22 +1,31 @@
 PennController.Sequence( randomize("picture") );
 PennController.ResetPrefix(null);
+PennController.AddHost("http://vfoss.org:9090/ibex/");
+// Run this before every trial
+PennController.Header(
+    newVar("score", 0)          // The score Var element is part of every trial
+        .settings.global()      // Global: its value is NOT reset each time
 );
+
+// INSTRUCTIONS
 PennController( "instructions" ,
     defaultText
         .print()
     ,
-    newText("intro1", "Hallo und willkommen zu unserer Studie!")
+    newText("intro1", "Welcome. You will see four patches of different colors aligned horizontally.")
     ,
-    newText("intro2", "In diesem Experiment wirst du Teile von Sätzen lesen, die zusammen mit vier Bildern erscheinen werden.")
+    newText("intro2", "You have 1 second to select the color whose name is shown above the line of patches.")
     ,
-    newText("intro3", "Du musst entschieden, welches von den vier Bildern den Satz am besten vervollständigen könnten. Du sollst versuchen, so schnell wie möglich zu antworten! Versuch dich also, auf deine Intuition zu verlassen.")
+    newText("intro3", "Place your fingers above the numeric keys 1, 2, 3 and 4 on your keyboard.")
     ,
-    newText("intro4", "Du wirst zwei Übungen dafür bekommen, bevor es mit dem eigentlichen Experiment losgeht. Klicke auf ‚weiter’ um mit den Übungen anzufangen.")
+    newText("intro4", "Press 1, 2, 3 or 4 to start.")
     ,
-    newButton("button", "weiter") // Handle exception: an all-digit string would be evaluated as a charCode
+    newKey("numeric", "1234ï¿½") // Handle exception: an all-digit string would be evaluated as a charCode
         .wait()                 // Start when 1, 2, 3 or 4 is pressed (weird character not on keyboard)
 )
-PennController.AddHost("http://vfoss.org:9090/ibex/");
+.setOption("hideProgressBar", true); // Do not show the progress bar on first screen
+
+
 PennController.Template( PennController.GetTable("itemlist.csv") ,
     row => PennController( "picture" ,
         defaultImage
