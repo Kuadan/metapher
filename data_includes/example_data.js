a89f077488c97b0debc25a2c52c375b0093f6303
Kuadan <tranphuongthao63@gmail.com>
2019-03-12 13:56:22 +0100

    test beispiel

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -6,26 +6,75 @@ PennController("instructions" ,
       .settings.center()
       .print()
     ,
-    newText("intro1", "Hallo und willkommen zu unserer Studie!")
+        newText("intro1", "Hallo und willkommen zu unserer Studie!")
     ,
-    newText("intro2", "In diesem Experiment wirst du Teile von Sätzen lesen, die zusammen mit vier Bildern erscheinen werden.")
+        newText("intro2", "In diesem Experiment wirst du Teile von Sätzen lesen, die zusammen mit vier Bildern erscheinen werden.")
         //.print()
     ,
-    newText("intro3", "Du musst entschieden, welches von den vier Bildern den Satz am besten vervollständigen könnten.")
+        newText("intro3", "Du musst entschieden, welches von den vier Bildern den Satz am besten vervollständigen könnten.")
         //.print()
     ,
-    newText("intro4", "Du sollst versuchen, so schnell wie möglich zu antworten! Versuch dich also, auf deine Intuition zu verlassen.")
+        newText("intro4", "Du sollst versuchen, so schnell wie möglich zu antworten! Versuch dich also, auf deine Intuition zu verlassen.")
     ,
-    newText("intro5", "Du wirst zwei Übungen dafür bekommen, bevor es mit dem eigentlichen Experiment losgeht.")
+        newText("intro5", "Du wirst zwei Übungen dafür bekommen, bevor es mit dem eigentlichen Experiment losgeht.")
     ,
-    newText("intro6", "Klicke auf ‚weiter’ um mit den Übungen anzufangen.")
+        newText("intro6", "Klicke auf ‚weiter’ um mit den Übungen anzufangen.")
         //.print()")
     ,
-    newButton("numeric", "weiter") // Handle exception: an all-digit string would be evaluated as a charCode
-        .print()
-        .wait()                 // Start when 1, 2, 3 or 4 is pressed (weird character not on keyboard)
+        newButton("button", "weiter") // Handle exception: an all-digit string would be evaluated as a charCode
+          .print()
+          .wait()                 // Start when 1, 2, 3 or 4 is pressed (weird character not on keyboard)
 )
 .setOption("hideProgressBar", true);
+
+PenController(
+      newText("example1", "Das Mädchen meint dass Köstlichkeit und ... absolut identisch sind")
+            .settings.css("font-size", "40pt")
+            .settings.css("padding-left", "380pt")
+            .print()
+  ,
+      newTimer("removeText",1500)
+            .start()
+            .wait()
+  ,
+      getText("test sentence")
+            .settings.text(" ")
+    //.remove()
+  ,
+      newImage("pic1","http://vfoss.org:9090/ibex/37_1_related_JPG.JPG")
+            .settings.size(640, 360)
+            //.print()
+  ,
+      newImage("pic2", "http://vfoss.org:9090/ibex/37_2_JPG.JPG")
+            .settings.size(640,360)
+            //.print()
+      ,
+      newImage("pic3","http://vfoss.org:9090/ibex/38_3_JPG.JPG")
+            .settings.size(640,360)
+            //.print()
+      ,
+      newImage("pic4", "http://vfoss.org:9090/ibex/38_4_JPG.JPG")
+            .settings.size(640,360)
+            //.print()
+      ,
+      newCanvas("tanks", 700, 400)
+            .settings.add(  0, 0, getImage("pic1") ) // 0 = left of canvas
+            .settings.add( 700, 0, getImage("pic2") )  // 300 = 100px to the right of the right edge of competitor
+            .settings.add(   0, 400, getImage("pic3") ) // 0 = left of canvas
+            .settings.add( 700, 400, getImage("pic4") )  // 300 = 100px to the right of the right edge of competitor
+            .print()
+  ,
+      newTimer("reminder", 4000)
+            .settings.callback( getText("test sentence").settings.text("Zu langsam!") )
+            .start()
+  ,
+      newSelector("tank")
+    //.settings.log()
+            .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
+            .settings.callback( getTimer("reminder").stop() )
+            .shuffle()
+            .wait()
+  )
 
 //PennController.Sequence( randomize("picture") );
 PennController.Template( PennController.GetTable("itemlist.csv"),
