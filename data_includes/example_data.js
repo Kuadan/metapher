5639d438881b0c9274c1bfd78a76add36f282900
Kuadan <tranphuongthao63@gmail.com>
2019-03-13 11:44:45 +0100

    back

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,12 +1,12 @@
 PennController.ResetPrefix(null);
 PennController.AddHost("http://vfoss.org:9090/ibex/");
-PennController.Sequence( "instructions1", "instructions2", randomize("picture1", "picture2") );
+PennController.Sequence( "instructions1", "picture1", "picture2", "instructions2", randomize("picture") );
 
 PennController("instructions1" ,
     defaultText
-      .settings.css("font-size", "25pt")
-      .settings.center()
-      .print()
+          .settings.css("font-size", "25pt")
+          .settings.center()
+          .print()
     ,
         newText("intro1", "Hallo und willkommen zu unserer Studie!")
     ,
@@ -21,143 +21,103 @@ PennController("instructions1" ,
         newText("intro6", "Klicke auf ‚weiter’ um mit den Übungen anzufangen.")
     ,
         newButton("button", "weiter")
-          .print()
-          .wait()
+            .print()
+            .wait()
 )
 .setOption("hideProgressBar", true);
 
-PennController.Template( PennController.GetTable("beispiel.csv"),
-    row => PennController( "picture1" ,
-        defaultImage
-            .settings.size(640, 360)
+PennController("picture1",
+      defaultImage
+              .settings.size(640, 360)
+     ,
+        newText("example1", "Das Mädchen meint liebt Süßgikeit und isst gern...")
+              .settings.css("font-size", "40pt")
+              .settings.center()
+              .print()
+    ,
+        newText("null"," ")
+    ,
+        newTimer("removeText1",1500)
+            .start()
+            .wait()
         ,
-        newText("test sentence1", row.sentence)
-            .settings.css("font-size", "40pt")
-            .settings.css("padding-left", "380pt")
-            .print()
+        getText("example1")
+            .settings.text("... Jetzt ein Bild auswählen..")
         ,
-        newText("null1"," ")
+        newImage("pic1","http://vfoss.org:9090/ibex/37_1_related_JPG.JPG")
         ,
-        newTimer("removeText1",3000)
-          .start()
-          .wait()
+        newImage("pic2", "http://vfoss.org:9090/ibex/37_2_JPG.JPG")
         ,
-        getText("test sentence1")
-          .settings.text(" ")
+        newImage("pic3","http://vfoss.org:9090/ibex/37_3_JPG.JPG")
+        ,
+        newImage("pic4", "http://vfoss.org:9090/ibex/37_4_JPG.JPG")
         ,
         newCanvas("tanks", 700, 400)
-          .settings.add(  0, 0, newImage("pic1", row.picture1) )
-          .settings.add( 700, 0, newImage("pic2", row.picture2) )
-          .settings.add(   0, 400, newImage("pic3", row.picture3) )
-          .settings.add( 700, 400, newImage("pic4", row.picture4) )
-          .print()
+            .settings.add(  0, 0, getImage("pic1") )
+            .settings.add( 700, 0, getImage("pic2") )
+            .settings.add(   0, 400, getImage("pic3") )
+            .settings.add( 700, 400, getImage("pic4") )
+            .print()
         ,
         newTimer("reminder1", 3000)
-          //.settings.callback( getText("test sentence").settings.text("Zu langsam!") )
-          .settings.callback( getSelector("tank").select(getText("null1")) )
-          .start()
+            .settings.callback( getSelector("tank").select(getText("null")) )
+            .settings.callback( getText("example1").settings.text("... zu langsam, next ")  )
+            .start()
         ,
         newSelector("tank")
-          //.settings.log()
-          .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
-          .settings.callback( getTimer("reminder1").stop() )
-          .shuffle()
-          .settings.log()
-          .wait()
-
-);
-//PennController("picture1",
-      //defaultImage
-            //.settings.size(640, 360)
-        //,
-        //newText("example1", "Das Mädchen meint liebt Süßgikeit und isst gern...")
-            //.settings.css("font-size", "40pt")
-            //.settings.center()
-            //.print()
-        //,
-        //newText("null"," ")
-        //,
-        //newTimer("removeText1",1500)
-            //.start()
-            //.wait()
-        //,
-        //getText("example1")
-            //.settings.text("... Jetzt ein Bild auswählen..")
-        //,
-        //newImage("pic1","http://vfoss.org:9090/ibex/37_1_related_JPG.JPG")
-        //,
-        //newImage("pic2", "http://vfoss.org:9090/ibex/37_2_JPG.JPG")
-        //,
-        //newImage("pic3","http://vfoss.org:9090/ibex/37_3_JPG.JPG")
-        //,
-        //newImage("pic4", "http://vfoss.org:9090/ibex/37_4_JPG.JPG")
-        //,
-        //newCanvas("tanks", 700, 400)
-            //.settings.add(  0, 0, getImage("pic1") )
-            //.settings.add( 700, 0, getImage("pic2") )
-            //.settings.add(   0, 400, getImage("pic3") )
-            //.settings.add( 700, 400, getImage("pic4") )
-            //.print()
-        //,
-        //newTimer("reminder1", 3000)
-            //.settings.callback( getSelector("tank").select(getText("null")) )
-            //.settings.callback( getText("example1").settings.text("... zu langsam, next ")  )
-            //.start()
-        //,
-        //newSelector("tank")
-           //.settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
-           //.settings.callback(  getTimer("reminder1").stop() )
-           //.shuffle()
+           .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
+           .settings.callback(  getTimer("reminder1").stop() )
+           .shuffle()
            //.settings.log()
-           //.wait()
-        //,
-        //newTimer("pause", 2000)
-               //.start()
-               //.wait()
-      //);
+           .wait()
+        ,
+        newTimer("pause", 2000)
+               .start()
+               .wait()
+      );
 
-//PennController("picture2",
-    //  defaultImage
-            //.settings.size(640, 360)
-      //,
-      //newText("example2", "Die Apothekerin bedauert dass Vollbeschäftigung und ... am Leben nicht zusammenzupassen scheinen.")
-            //.settings.css("font-size", "40pt")
-            //.settings.center()
-            //.print()
-      //,
-      //newText("null"," ")
-      //,
-      //newTimer("removeText2",1500)
-            //.start()
-            //.wait()
-      //,
-      //getText("example2")
-            //.settings.text("... Jetzt ein Bild auswählen..")
-      //,
-      //newImage("pic5","http://vfoss.org:9090/ibex/38_1_JPG.JPG")
-      //,
-      //newImage("pic6", "http://vfoss.org:9090/ibex/38_2_JPG.JPG")
-      //,
-      //newImage("pic7","http://vfoss.org:9090/ibex/38_3_JPG.JPG")
-      //,
-      //newImage("pic8", "http://vfoss.org:9090/ibex/38_4_JPG.JPG")
-      //,
-      //newCanvas("tanks", 700, 400)
-            //.settings.add(  0, 0, getImage("pic5") )
-            //.settings.add( 700, 0, getImage("pic6") )
-            //.settings.add(   0, 400, getImage("pic7") )
-            //.settings.add( 700, 400, getImage("pic8") )
-            //.print()
-      //,
-      //newTimer("reminder2", 3000)
-          //.settings.callback( getSelector("tank").select(getText("null")) )
-          //.settings.callback( getText("example1").settings.text("... zu langsam, next ")  )
-          //.start()
-      //,
-     //newSelector("tank")
-           //.settings.add( getImage("pic5") , getImage("pic6"), getImage("pic7"), getImage("pic8") )
-           //.wait()
-       //);
+PennController("picture2",
+     defaultImage
+            .settings.size(640, 360)
+      ,
+      newText("example2", "Die Apothekerin arbeitet viel und braucht ...")
+            .settings.css("font-size", "40pt")
+            .settings.center()
+            .print()
+      ,
+      newText("null"," ")
+      ,
+      newTimer("removeText2",1500)
+            .start()
+            .wait()
+      ,
+      getText("example2")
+            .settings.text("... Jetzt ein Bild auswählen..")
+      ,
+    newImage("pic5","http://vfoss.org:9090/ibex/38_1_JPG.JPG")
+      ,
+      newImage("pic6", "http://vfoss.org:9090/ibex/38_2_JPG.JPG")
+      ,
+      newImage("pic7","http://vfoss.org:9090/ibex/38_3_JPG.JPG")
+      ,
+      newImage("pic8", "http://vfoss.org:9090/ibex/38_4_JPG.JPG")
+      ,
+      newCanvas("tanks", 700, 400)
+            .settings.add(  0, 0, getImage("pic5") )
+            .settings.add( 700, 0, getImage("pic6") )
+            .settings.add(   0, 400, getImage("pic7") )
+            .settings.add( 700, 400, getImage("pic8") )
+            .print()
+      ,
+      newTimer("reminder2", 3000)
+          .settings.callback( getSelector("tank").select(getText("null")) )
+          .settings.callback( getText("example1").settings.text("... zu langsam, next ")  )
+          .start()
+      ,
+     newSelector("tank")
+           .settings.add( getImage("pic5") , getImage("pic6"), getImage("pic7"), getImage("pic8") )
+           .wait()
+       );
 
 PennController("instructions2" ,
     defaultText
