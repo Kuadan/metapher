d8fcd49be4f934ad5f644259379c4d383d2b2a49
Kuadan <tranphuongthao63@gmail.com>
2019-04-24 11:14:19 +0200

    edit list, add link

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -1,11 +1,10 @@
 PennController.ResetPrefix(null);
 PennController.AddHost("http://vfoss.org:9090/ibex/");
-PennController.Sequence( "instructions1", "picture1", "picture2", "picture3", "instructions2", randomize("picture") );
-
+PennController.Sequence( "instructions1", "picture1", "picture2", "picture3", "instructions2", randomize("items") );
 PennController("instructions1" ,
     defaultText
           .settings.css("font-size", "25pt")
-          .settings.center()
+          .settings.left()
           .print()
         ,
         newText("intro1", "Hallo und willkommen zu unserer Studie!")
@@ -14,9 +13,9 @@ PennController("instructions1" ,
         ,
         newText("intro3", "Du musst dich entscheiden, welches von den vier Bildern den Satz am besten vervollständigen könnte.")
         ,
-        newText("intro4", "Du sollst versuchen, so schnell wie möglich zu antworten! Versuch dich also, auf deine Intuition zu verlassen.Wenn du nicht schnell genug bist, wird eine Meldung auf dem Bildschirm gezeigt.")
+        newText("intro4", "Du sollst so schnell wie möglich antworten! Versuch dich also auf deine Intuition zu verlassen. Wenn du nicht schnell genug bist, wird eine Meldung auf dem Bildschirm gezeigt.")
         ,
-        newText("intro5", "Du wirst drei Übungen dafür bekommen, bevor es mit dem eigentlichen Experiment losgeht.")
+        newText("intro5", "Du wirst drei Übungsbeispiele bekommen, bevor es mit dem eigentlichen Experiment losgeht.")
         ,
         newText("intro6", "Klicke auf ‚weiter’ um mit den Übungen anzufangen.")
         ,
@@ -30,19 +29,19 @@ PennController("picture1",
         defaultImage
               .settings.size(640, 360)
           ,
-          newText("example1", "Das Mädchen liebt Süßgikeit und isst gern...")
+          newText("example1", "Das Mädchen isst gerne...")
               .settings.css("font-size", "40pt")
-              .settings.css("padding-left", "200pt")
+              .settings.css("padding-left", "1pt")
               .print()
           ,
           newText("null"," ")
           ,
-          newTimer("removeText1",2000)
+          newTimer("removeText1",3000)
               .start()
               .wait()
           ,
           getText("example1")
-              .settings.text("... Jetzt ein Bild auswählen..")
+              .settings.text(" ")
           ,
           newImage("pic1","http://vfoss.org:9090/ibex/37_1_related_JPG.JPG")
           ,
@@ -52,50 +51,67 @@ PennController("picture1",
           ,
           newImage("pic4", "http://vfoss.org:9090/ibex/37_4_JPG.JPG")
           ,
-          newCanvas("tanks", 700, 400)
-              .settings.add(  0, 0, getImage("pic1") )
-              .settings.add( 700, 0, getImage("pic2") )
-              .settings.add(   0, 400, getImage("pic3") )
-              .settings.add( 700, 400, getImage("pic4") )
+          newCanvas("tanks", 700, 500)
+              .settings.add(  -330, 20, getImage("pic1") )
+              .settings.add( -350, 440, getImage("pic2") )
+              .settings.add(   350, 20, getImage("pic3") )
+              .settings.add( 350, 440, getImage("pic4") )
               .print()
           ,
           // nichts gewählt
           newTimer("reminder1", 3000)
               .settings.callback( getSelector("tank").select(getText("null")) )
-              .settings.callback( getText("example1").settings.text("... zu langsam, next ")  )
+              .settings.callback( getText("example1").settings.text("... Sei bitte schneller! ")  )
               .start()
           ,
           newSelector("tank")
               .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
               .settings.callback(  getTimer("reminder1").stop() )
-              .settings.callback( getText("example1").settings.text("Schnell genug!")  )
+              .settings.callback( getText("example1").settings.text(" ")  )
               .shuffle()
            //.settings.log()
               .wait()
           ,
-          newTimer("pause1",1000)
-               .start()
-               .wait()
+          getCanvas("tanks")
+            .remove()
+      ,
+          newText("pleasewait", "Gehe zum nächsten Satz")
+          .settings.css("font-size", "20pt")
+          .settings.css("margin-top", "250pt")
+          .settings.css("padding-left", "20pt")
+          .print()
+      ,
+      newTimer("wait", 10)
+          .start()
+          .wait()
+      ,
+      newButton("button", "weiter")
+          .print()
+          .wait()
+      ,
+      getText("pleasewait")
+          .remove()
+
       );
 
 PennController("picture2",
-     defaultImage
-              .settings.size(640, 360)
-          ,
-          newText("example2", "Die Apothekerin arbeitet viel und braucht ...")
-              .settings.css("font-size", "40pt")
-              .settings.css("padding-left", "200pt")
-              .print()
-          ,
-          newText("null"," ")
-          ,
-          newTimer("removeText2",2000)
-              .start()
-              .wait()
-          ,
-          getText("example2")
-              .settings.text("... Jetzt ein Bild auswählen..")
-          ,
+           defaultImage
+                    .settings.size(640, 360)
+                ,
+                newText("example2", "Der Apotheker macht Sport im ...")
+                    .settings.css("font-size", "40pt")
+                    .settings.css("padding-left", "1pt")
+                    .print()
+                ,
+                newText("null"," ")
+                ,
+                newTimer("removeText2",2000)
+                    .start()
+                    .wait()
+                ,
+                getText("example2")
+                    .settings.text(" ")
+                ,
           newImage("pic5","http://vfoss.org:9090/ibex/39_1_JPG.JPG")
           ,
           newImage("pic6", "http://vfoss.org:9090/ibex/38_2_JPG.JPG")
@@ -104,47 +120,64 @@ PennController("picture2",
           ,
           newImage("pic8", "http://vfoss.org:9090/ibex/38_4_JPG.JPG")
           ,
-          newCanvas("tanks", 700, 400)
-              .settings.add(  0, 0, getImage("pic5") )
-              .settings.add( 700, 0, getImage("pic6") )
-              .settings.add(   0, 400, getImage("pic7") )
-              .settings.add( 700, 400, getImage("pic8") )
+          newCanvas("tanks", 700, 500)
+              .settings.add(  -330, 20, getImage("pic5") )
+              .settings.add( -350, 440, getImage("pic6") )
+              .settings.add(   350, 20, getImage("pic7") )
+              .settings.add( 350, 440, getImage("pic8") )
               .print()
           ,
           newTimer("reminder2", 3000)
               .settings.callback( getSelector("tank").select(getText("null")) )
-              .settings.callback( getText("example2").settings.text("... zu langsam, next ")  )
+              .settings.callback( getText("example2").settings.text("Sei bitte schneller!")  )
               .start()
           ,
           newSelector("tank")
               .settings.add( getImage("pic5") , getImage("pic6"), getImage("pic7"), getImage("pic8") )
               .settings.callback( getTimer("reminder2").stop() )
-              .settings.callback( getText("example2").settings.text("Schnell genug!")  )
+              .settings.callback( getText("example2").settings.text(" ")  )
               .shuffle()
               .wait()
         ,
-          newTimer("pause2", 1000)
-              .start()
-              .wait()
+        getCanvas("tanks")
+          .remove()
+    ,
+    newText("pleasewait", "Gehe zum nächsten Satz")
+    .settings.css("font-size", "20pt")
+    .settings.css("padding-left", "1pt")
+    .settings.css("margin-top", "250pt")
+    .print()
+    ,
+    newTimer("wait", 10)
+      .start()
+      .wait()
+,
+    newButton("button", "weiter")
+      .print()
+      .wait()
+,
+    getText("pleasewait")
+    .remove()
+
        );
 
- PennController("picture3",
-             defaultImage
-                     .settings.size(640, 360)
-                 ,
-                 newText("example3", "Die Bäckerin backt Kuchen mit ...")
-                     .settings.css("font-size", "40pt")
-                     .settings.css("padding-left", "200pt")
-                     .print()
-                 ,
-                 newText("null"," ")
-                 ,
-                 newTimer("removeText3",2000)
-                     .start()
-                     .wait()
-                 ,
-                 getText("example3")
-                     .settings.text("... Jetzt ein Bild auswählen..")
+       PennController("picture3",
+                   defaultImage
+                           .settings.size(640, 360)
+                       ,
+                       newText("example3", "Der Urlauber schwimmt im...")
+                           .settings.css("font-size", "40pt")
+                           .settings.css("padding-left", "1pt")
+                           .print()
+                       ,
+                       newText("null"," ")
+                       ,
+                       newTimer("removeText3",2000)
+                           .start()
+                           .wait()
+                       ,
+                       getText("example3")
+                           .settings.text(" ")
                  ,
                  newImage("pic9","http://vfoss.org:9090/ibex/38_2_JPG.JPG")
                  ,
@@ -154,95 +187,133 @@ PennController("picture2",
                  ,
                  newImage("pic12", "http://vfoss.org:9090/ibex/48_4_JPG.JPG")
                  ,
-                 newCanvas("tanks", 700, 400)
-                     .settings.add(  0, 0, getImage("pic9") )
-                     .settings.add( 700, 0, getImage("pic10") )
-                     .settings.add(   0, 400, getImage("pic11") )
-                     .settings.add( 700, 400, getImage("pic12") )
+                 newCanvas("tanks", 700, 500)
+                     .settings.add(  -330, 20, getImage("pic9") )
+                     .settings.add( -350, 440, getImage("pic10") )
+                     .settings.add(   350, 20, getImage("pic11") )
+                     .settings.add( 350, 440, getImage("pic12") )
                      .print()
                  ,
                  newTimer("reminder3", 3000)
                      .settings.callback( getSelector("tank").select(getText("null")) )
-                     .settings.callback( getText("example3").settings.text("... zu langsam, next ")  )
+                     .settings.callback( getText("example3").settings.text("Sei bitte schneller!")  )
                      .start()
                  ,
                  newSelector("tank")
                      .settings.add( getImage("pic9") , getImage("pic10"), getImage("pic11"), getImage("pic12") )
                      .settings.callback(  getTimer("reminder3").stop() )
-                     .settings.callback( getText("example3").settings.text("Schnell genug!")  )
+                     .settings.callback( getText("example3").settings.text(" ")  )
                      .shuffle()
                   //.settings.log()
                      .wait()
                  ,
-                 newTimer("pause3", 1000)
-                      .start()
-                      .wait()
+                 getCanvas("tanks")
+                   .remove()
+               ,
+               newText("pleasewait", "Gehe zum nächsten Satz")
+               .settings.css("font-size", "20pt")
+               .settings.css("margin-top", "220pt")
+               .settings.css("padding-left", "1pt")
+               .print()
+               ,
+               newTimer("wait", 10)
+                 .start()
+                 .wait()
+           ,
+               newButton("button", "weiter")
+                 .print()
+                 .wait()
+           ,
+               getText("pleasewait")
+               .remove()
+
              );
 
-PennController("instructions2" ,
-    defaultText
-          .settings.css("font-size", "25pt")
-          .settings.center()
-          .print()
-    ,
-        newText("intro1", "Das war die Übung!")
-    ,
-        newText("intro2", "Jetzt werden wir mit dem Experiment starten.")
-    ,
-        newText("intro3", "Wenn du noch Fragen hast, wende dich bitte jetzt an die Experimentleiterin.")
-    ,
-        newText("intro4", "Ansonsten kannst du auf ‚weiter’ klicken.")
-    ,
-        newButton("button", "weiter")
-          .print()
-          .wait()
-)
-.setOption("hideProgressBar", true);
+             PennController("instructions2" ,
+                 defaultText
+                       .settings.css("font-size", "25pt")
+                       .settings.left()
+                       .settings.css("margin-top", "50pt")
+                       .print()
+                 ,
+                     newText("intro1", "Das war die Übung!")
+                 ,
+                     newText("intro2", "Jetzt werden wir mit dem Experiment starten.")
+                 ,
+                     newText("intro3", "Wenn du noch Fragen hast, wende dich bitte jetzt an die Experimentleiterin.")
+                 ,
+                     newText("intro4", "Ansonsten kannst du auf ‚weiter’ klicken.")
+                 ,
+                     newButton("button", "weiter")
+                       .print()
+                       .wait()
+             )
+             .setOption("hideProgressBar", true);
 
 
-PennController.Template( PennController.GetTable("itemlist.csv"),
-    row => PennController( "picture" ,
-        defaultImage
-            .settings.size(640, 360)
-        ,
-        newText("test sentence", row.sentence)
-            .settings.css("font-size", "40pt")
-            .settings.css("padding-left", "380pt")
-            .print()
-        ,
-        newText("null"," ")
-        ,
-        newTimer("removeText",1000)
-          .start()
-          .wait()
-        ,
-        getText("test sentence")
-          .settings.text(" ")
-        ,
-        newCanvas("tanks", 700, 400)
-          .settings.add(  0, 0, newImage("pic1", row.metaphoric) )
-          .settings.add( 700, 0, newImage("pic2", row.literal) )
-          .settings.add(   0, 400, newImage("pic3", row.distr1) )
-          .settings.add( 700, 400, newImage("pic4", row.distr2) )
-          .print()
-        ,
-        newTimer("reminder", 3000)
-          //.settings.callback( getText("test sentence").settings.text("Zu langsam!") )
-          .settings.callback( getSelector("tank").select(getText("null")) )
-          .settings.callback( getText("test sentence").settings.text("Zu langsam!") )
-          .start()
-        ,
-        newSelector("tank")
-          //.settings.log()
-          .settings.add( getImage("pic1") , getImage("pic2"), getImage("pic3"), getImage("pic4") )
-          .settings.callback( getTimer("reminder").stop() )
-          .settings.callback( getText("test sentence").settings.text("Schnell genug!")  )
-          .shuffle()
-          .settings.log()
-          .wait()
-       ,
-       newTimer("pause", 1000)
-          .start()
-          .wait()
-        )
-);
+             PennController.Template( PennController.GetTable("itemlist.csv"),
+                 row => PennController( "items" ,
+                     defaultImage
+                         .settings.size(640, 360)
+                     ,
+                     newText("test sentence", row.sentence)
+                         .settings.css("font-size", "40pt")
+                         .settings.css("padding-left", "1pt")
+                         .print()
+                     ,
+                     newText("null"," ")
+                     ,
+                     newTimer("removeText",2000)
+                       .start()
+                       .wait()
+                     ,
+                     getText("test sentence")
+                       .settings.text(" ")
+                     ,
+                     newCanvas("tanks", 700, 500)
+                       .settings.add(  -330, 20, newImage("target", row.metaphoric) )
+                       .settings.add( -350, 440, newImage("competitor", row.literal) )
+                       .settings.add(   350, 20, newImage("distractor1", row.distr1) )
+                       .settings.add( 350, 440, newImage("distractor2", row.distr2) )
+                       .print()
+                       .settings.log()
+
+                     ,
+                     newTimer("reminder", 3000)
+                     //  .settings.callback( getText("test sentence").settings.text("Sei bitte schneller!") )
+                       .settings.callback( getSelector("tank").select(getText("null")) )
+                       .settings.callback( getText("test sentence").settings.text("Sei bitte schneller!") )
+                       .start()
+                     ,
+                     newSelector("tank")
+                       //.settings.log()
+                       .settings.add( getImage("target") , getImage("competitor"), getImage("distractor1"), getImage("distractor2") )
+                       .settings.callback( getTimer("reminder").stop() )
+                       .settings.callback( getText("test sentence").settings.text(" ")  )
+                       .shuffle()
+                       .settings.log()
+                       .wait()
+                    ,
+                    getCanvas("tanks")
+                      .remove()
+                    ,
+                    newText("pleasewait", "Gehe zum nächsten Satz")
+                       .settings.css("font-size", "20pt")
+                       .settings.css("padding-left", "1pt")
+                       .settings.css("margin-top", "250pt")
+                       .print()
+                   ,
+                   newTimer("wait", 10)
+                       .start()
+                       .wait()
+                   ,
+                   newButton("button", "weiter")
+                    .print()
+                    .wait()
+              ,
+                  getText("pleasewait")
+                  .remove()
+
+                     )
+                     .log( "item" , row.item )
+             );
